b5c019fdc50ee9f299475fc347dd42de2cf3640b
nopekun <51075690+toufiqnuur@users.noreply.github.com>
2020-06-13 14:40:08 +0700

    Create Yesterday.dart

---
+++ ngeApps/iTalks/Yesterday.dart
@@ -0,0 +1,11 @@
+var container = Container( // grey box
+  child:  Center(
+    child:  Text(
+      "Lorem ipsum",
+      style: bold24Roboto,
+    ),
+  ),
+  width: 320,
+  height: 240,
+  color: Colors.grey[300],
+);
