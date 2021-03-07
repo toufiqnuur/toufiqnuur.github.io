e54077085d50582377f2503f36fbe22bb28f1a0e
toufiqnuur <taufik2017yk@gmail.com>
2021-03-07 08:34:57 +0700

    page

--- js/main.js
+++ js/main.js
@@ -1,32 +1,45 @@
-class Tabbar {
-        constructor(props) {
-          this.props = document.querySelectorAll(props)
-        }
-        enable(index, props) {
-          this.current = this.props[index]
-          this.props.forEach(tab => {
-            tab.onclick = () => {
-              if(this.current) {
-                this.current.classList.remove(props)
-              }
-              this.current = tab
-              this.current.classList.add(props)
-            }
-          })
-        }
-      }
-      let bottomBar, mediumBar;
-      bottomBar = new Tabbar('.navbar.fixed-bottom a')
-      mediumBar = new Tabbar('.navbar .nav-link')
-      bottomBar.enable(0,'actived')
-      mediumBar.enable(null,'actived')
+class Highlight {
+  constructor(props) {
+    this.target = document.querySelectorAll(props)
+  }
+  active(props, path) {
+    this.location = /\w+/.exec(window.location.pathname)
+    if (this.location == null) {
+      this.target[0].classList.add(props)
+    }
+    this.target[path[this.location]].classList.add(props)
+  }
+}
+
+let bottomBar, mediumBar;
+
+bottomBar = new Highlight('.navbar.fixed-bottom a')
+bottomBar.active('actived', {
+  'arsip': 1,
+  'topic': 2,
+  'search': 3
+})
+
+mediumBar = new Highlight('.navbar .nav-link')
+mediumBar.active('actived', {
+  'arsip': 0,
+  'topic': 1,
+  'search': 2
+})
+
 
 let byMonth, byYear
 byMonth = document.querySelector('#month')
 byYear = document.querySelector('#year')
 arsipList = document.querySelector('#arsip-list')
 
+arsipContent = document.querySelectorAll('li.arsip')
+
 byMonth.onchange = event => {
-  arsipList.innerHTML = event.target.value
-  //arsipList.innerHTML = arsipList.children
+  arsipList.innerHTML = ''
+  arsipContent.forEach(arsip => {
+    if (arsip.dataset.key.split(' ')[0] == event.target.value) {
+      arsipList.appendChild(arsip)
+    }
+  })
 }
